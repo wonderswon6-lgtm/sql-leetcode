358243d0156cc595036bb27ba7a4ca464586401b
Sahana Srinivasa <wonderswon6@gmail.com>
2026-08-05 07:32:53 +0530

    Time: 2111 ms (36.03%) | Memory: 13.3 MB (36.15%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums, target):
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(i + 1, n):
+
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
