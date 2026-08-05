c997dfb7944d74b6e04067883e549eebad68f817
Sahana Srinivasa <wonderswon6@gmail.com>
2026-08-05 07:47:13 +0530

    Time: 500 ms (18.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+
+WITH PersonAddress AS
+(
+SELECT
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state
+FROM Person p
+LEFT JOIN Address a
+ON p.personId=a.personId
+)
+
+SELECT *
+FROM PersonAddress;
